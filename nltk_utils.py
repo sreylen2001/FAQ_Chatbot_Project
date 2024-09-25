4642ad7f8a5af4e8925d244cc7f73b12dc718e8d
sreylen2001 <98254955+sreylen2001@users.noreply.github.com>
2024-09-25 16:02:42 +0700

    add-new-ui

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,34 +1,44 @@
+import numpy as np
 import nltk
-nltk.download('punkt')
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
-import numpy as np
+
 
 def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
     return nltk.word_tokenize(sentence)
 
+
 def stem(word):
-    return stemmer.stem(word.lower()) # to covert the word to lowercase
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
 
-def bag_of_words(tokenize_sentence, all_words):
-    tokenize_sentence = [stem(w) for w in tokenize_sentence]
-    bag = np.zeros(len(all_words), dtype=np.float32)
-    for idx, w in enumerate(all_words):
-        if w in tokenize_sentence:
-            bag[idx] = 1.0
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
     return bag
-
-
-# sentence = ["hello", "how", "are", "you"]
-# words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-# bog = bag_of_words(sentence, words)
-# print(bog)
-
-# a = "How are you doing?"
-# print(a)
-# a = tokenize(a)
-# print(a)
-
-# word = ["Organize", "organizes", "organizing"]
-# stemmed_words = [stem(w) for w in words]
-# print(stemmed_words)
