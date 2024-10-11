d4f2c2ad3574e67012b7c680010d28818f765165
sreylen2001 <98254955+sreylen2001@users.noreply.github.com>
2024-10-11 16:00:55 +0700

    add-new-intents

--- train.py
+++ train.py
@@ -54,11 +54,11 @@ X_train = np.array(X_train)
 y_train = np.array(y_train)
 
 # Hyper-parameters 
-num_epochs = 1000
-batch_size = 8
-learning_rate = 0.001
+num_epochs = 500
+batch_size = 32
+learning_rate = 0.005
 input_size = len(X_train[0])
-hidden_size = 8
+hidden_size = 64
 output_size = len(tags)
 print(input_size, output_size)
 
